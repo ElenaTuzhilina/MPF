e8c692a4812b9f1a4170b7539ea826f5aafb252f
Elena Tuzhilina <elenatuz@stanford.edu>
2022-08-27 18:15:29 -0400

    simulation

---
+++ functions.R
@@ -0,0 +1,174 @@
+
+########## method ########## 
+
+compute_xy = function(X, Y, H){
+  index = (rowSums(is.na(X))==0)
+  X = X[index,]
+  Y = Y[index,]
+  y = c(Y)
+  x = H %x% X
+  x = x[!is.na(y),]
+  y = y[!is.na(y)]
+  w = rep(1, length(y))
+  return(list(x = x, y = y, w = w))
+}
+
+mpf = function(X, Y, H){
+  reshape = compute_xy(X, Y, H)
+  theta = lm(reshape$y ~ reshape$x + 0, weights = reshape$w)$coef
+  Theta = matrix(theta, nrow = ncol(H), byrow = TRUE)
+  B = t(H %*% Theta)
+  return(list(Theta = Theta, B = B))
+} 
+
+mrr = function(X, Y){
+  B = matrix(0, ncol(X), ncol(Y))
+  for(i in 1:ncol(Y)){
+    y = Y[, i]
+    x = X[!is.na(y),]
+    y = y[!is.na(y)]
+    B[, i] = lm(y ~ x + 0)$coef
+  }
+  return(list(B = B))
+} 
+
+########## basis ##########
+
+compute_H = function(time_stamp, aheads, d){
+  H = matrix(1/sqrt(length(aheads)), length(aheads), 1)
+  if(d > 1 & d < length(aheads)) H = cbind(H, poly(time_stamp + aheads, df = d - 1, raw = F))
+  if(d == length(aheads)) H = diag(d)
+  return(H)
+}
+
+########## data preprocessing ##########
+
+create_data = function(df, signals, aheads, lags, top_geo){
+  list(Y = df %>% 
+         filter(geo_value %in% top_geo) %>%
+         select(paste0(signals[1], "+", aheads)) %>%
+         as.matrix(),
+       X = df %>% 
+         filter(geo_value %in% top_geo) %>%
+         select(as.vector(outer(signals, lags, paste, sep = "-"))) %>% 
+         mutate("intercept" = 1) %>%
+         as.matrix(),
+       info = df %>% 
+         filter(geo_value %in% top_geo) %>%
+         select(geo_value, time_stamp),
+       stamps = unique(df$time_stamp))
+}
+
+remove_missing = function(df){
+  index <- (rowSums(is.na(df$Y))==0)
+  list(Y = df$Y[index,],
+       X = df$X[index,],
+       info = df$info[index,],
+       stamps = df$stamps[index])
+}
+
+reshape_y = function(Y, info){
+  aheads <- find_aheads(Y)
+  cbind(Y, info) %>% 
+    gather(1:ncol(Y), key = "ahead", value = "cases") %>%
+    mutate(ahead = factor(ahead, levels = paste0('confirmed_7dav_incidence_prop+', aheads), labels = aheads)) 
+}
+
+find_top_geo <- function(df, n){
+  df <- na.omit(df)
+  df %>% 
+    group_by(geo_value) %>% 
+    summarise_at(vars(contains("+")), ~sum(., na.rm = T))  %>%
+    mutate(total_cases = rowSums(select(., contains("+")), na.rm = TRUE)) %>%
+    arrange(desc(total_cases)) %>%
+    top_n(n, total_cases) %>% 
+    select(geo_value) %>%
+    pull()
+}
+
+find_aheads = function(Y){
+  as.numeric(substr(colnames(test$Y), nchar(signals[1])+2, nchar(signals[1])+3))
+}
+
+########## plots ##########
+
+plot_missing = function(Y, info, Ytest, infotest){
+  aheads <- find_aheads(Y)
+  stamps <- unique(info$time_stamp)
+  sub <- info$geo_value == info$geo_value[1]
+  nas <- reshape_y(Y[sub,], info[sub,]) %>% mutate(missing = is.na(cases), set = "observed train")
+  aheadstest <- find_aheads(Ytest)
+  stampstest <- unique(infotest$time_stamp)
+  subtest <- infotest$geo_value == info$geo_value[1]
+  nastest <- reshape_y(Ytest[subtest,], infotest[subtest,]) %>% mutate(missing = is.na(cases), set = "unobserved test")
+  ggplot(rbind(nas, nastest))+
+    geom_tile(aes(as.numeric(ahead), time_stamp, fill = interaction(set, missing)))+ 
+    #scale_fill_manual(values=c("dodgerblue4", "tomato4"))+
+    scale_fill_manual(name = "legend",
+                      values = c(
+                        alpha("dodgerblue4", 1),
+                        alpha("tomato4", 1),
+                        alpha("dodgerblue4", 0.1)),
+                      labels = c("observed train", 
+                                 "unobserved test",
+                                 "unobserved train"))+
+    scale_x_continuous(breaks = seq(min(aheads), max(aheads), 5))+
+    scale_y_continuous(breaks = max(stampstest) - seq(0, as.numeric(max(stampstest) - min(stamps)), 7))+
+    xlab("ahead")+
+    coord_flip()+
+    theme_bw()+
+    theme(legend.position="top")+
+    theme(legend.title = element_blank())+
+    theme(axis.text.x = element_text(size=8, angle=30, hjust = 1))
+}
+
+plot_fit = function(test, model, geo_values, stamps, pltname){
+  Yhat_test <- test$X %*% model$B
+  colnames(Yhat_test) <- colnames(test$Y)
+  Y <- rbind(data.frame(reshape_y(test$Y, test$info), "type" = "observed"),
+                data.frame(reshape_y(Yhat_test, test$info), "type" = "predicted")) %>% 
+    mutate(type = factor(type, levels = c("predicted", "observed"))) %>%
+    mutate(ahead = as.numeric(ahead)) %>%
+    mutate(time = time_stamp + ahead) %>%
+    filter(geo_value %in% geo_values) %>% 
+    filter(time_stamp %in% stamps)
+  ggplot(Y)+
+    geom_line(aes(ahead, cases, color = type, group = interaction(time_stamp, type)))+
+    facet_grid(time_stamp~geo_value, scales = "free")+
+    theme_light()+
+    theme(legend.position="top", legend.title = element_blank())+
+    scale_color_manual(values=c("tomato1", "tomato4"))
+  ggsave(pltname, height = length(stamps) * 1.2, width = length(geo_values) * 1.5)
+}
+
+plot_fit_merged = function(train, test, model, geo_values, pltname){
+  Yhat_train <- train$X %*% model$B
+  colnames(Yhat_train) <- colnames(train$Y)
+  Yhat_test <- test$X %*% model$B
+  colnames(Yhat_test) <- colnames(test$Y)
+  Y <- rbind(data.frame(reshape_y(train$Y, train$info), "type" = "observed train"),
+             data.frame(reshape_y(Yhat_train, train$info), "type" = "predicted train"),
+             data.frame(reshape_y(test$Y, test$info), "type" = "unobserved test"),
+             data.frame(reshape_y(Yhat_test, test$info), "type" = "predicted test")) %>% 
+    mutate(type = factor(type, levels = c("predicted train", "predicted test", "observed train", "unobserved test"))) %>%
+    mutate(ahead = as.numeric(ahead)) %>%
+    mutate(time = time_stamp + ahead) %>%
+    filter(geo_value %in% geo_values) %>%
+    filter(type %in% c("observed train", "predicted test")) %>%
+    filter(time_stamp <= as.Date("2021-10-02")) %>%
+    mutate(geo_value = paste0("Pierce County"))
+  ggplot(Y, aes(time, cases, color = type, size = type, alpha = type, group = interaction(time_stamp, type)))+
+    geom_line()+
+    geom_point()+
+    facet_grid(~geo_value)+
+    theme_light()+
+    theme(legend.position="top")+
+    theme(legend.title = element_blank())+
+    scale_size_manual(values=c( "predicted test" = 0.3, "observed train" = 0.3))+
+    scale_color_manual(values=c("predicted test" = "tomato1", "observed train" = "dodgerblue1"))+
+    scale_alpha_manual(values=c("predicted test" = 1, "observed train" = 1))
+  ggsave(pltname, height = 3, width = length(geo_values) * 5)
+  return(Y)
+}
+
+
---
+++ simulation.R
@@ -0,0 +1,82 @@
+source("functions.R")
+library(dplyr)
+library(ggplot2)
+
+set.seed(0)
+
+n = 1000 #location
+q = 30 #ahead
+A = 1:q
+p = 10 #number of predictors
+d = 3 #spline basis size
+m = p #assume all lags = 0
+
+#generate the data
+generate = function(n, q, p, d, A){
+  X = matrix(rnorm(n * p), n, p)
+  H = compute_H(0, A, d)
+  Theta = matrix(rnorm(ncol(H) * m), ncol(H), m) #spline coefficients
+  B = t(H %*% Theta) #smooth model coefficients
+  Y = X %*% B #response
+  W = (runif(length(Y)) <= 0.1) #10% of missing values
+  Y[W] = NA
+  return(list(Y = Y, X = X))
+}
+
+#run simulation for df = 1...6
+result = c()
+dfs = 1:6
+
+for(rep in 1:100){
+  cat(rep, " ")
+  #generate the data
+  gen = generate(1000, 30, 10, 3, 1:30)
+  X = gen$X
+  Y = gen$Y
+  #train-test split
+  index = sample(1:n, n*0.5)
+  for(SNR in c(0.1, 0.5, 1, 2)){
+    #compute sd for errors
+    s = abs(mean(Y, na.rm = TRUE)/SNR)
+    E = matrix(rnorm(length(Y), 0, s), dim(Y))
+    for(d in dfs){
+      #create basis
+      H = compute_H(0, A, d)
+      #find mof solution
+      sol = mpf(X[index,], (Y+E)[index,], H)
+      #calculate train-test scores
+      train = mean(abs(Y[index,] - X[index,] %*% sol$B), na.rm = T)
+      test = mean(abs(Y[-index,] - X[-index,] %*% sol$B), na.rm = T)
+      res = data.frame(df = d, score = c(train, test), score_type = c("train", "test"))
+      result = rbind(result, data.frame(res, s, SNR, model = "smooth", rep = rep))
+    }
+    #find the solution when applying separate regressions
+    H = compute_H(0, A, q)
+    sol = mpf(X[index,], (Y+E)[index,], H)
+    train = mean(abs(Y[index,] - X[index,] %*% sol$B), na.rm = T)
+    test = mean(abs(Y[-index,] - X[-index,] %*% sol$B), na.rm = T)
+    res = data.frame(df = d, score = c(train, test), score_type = c("train", "test"))
+    result = rbind(result, data.frame(df = rep(dfs, times = rep(2, length(dfs))), score = res$score, score_type = res$score_type, s, SNR, model = "baseline", rep = rep))
+  }
+}
+
+
+#compute average score and confidence intervals
+result_avg = result %>% 
+  group_by(df, score_type, SNR, model) %>% 
+  mutate(mean = mean(score), se = sd(score)/sqrt(100), lower = quantile(score, 0.05), upper = quantile(score, 0.95))
+           
+#plot train-test performance
+ggplot(data = result_avg %>% filter(score_type == "test"), mapping = aes(x = df, y = log(mean, 10), color = model, fill = model, linetype = model))+
+  geom_line(size = 0.5)+
+  geom_ribbon(aes(x = df, y = log(mean, 10), ymax = log(mean +  se, 10), ymin = log(mean - se, 10)), alpha=0.3, color = NA)+
+  facet_wrap(~SNR, ncol = 5, labeller = label_bquote(cols = SNR == .(SNR)))+ 
+  xlab("degrees-of-freedom") + ylab("log(test mae)")+
+  scale_x_continuous(breaks = dfs)+
+  labs(color = "type", linetype = "type")+
+  theme_bw()+
+  theme(legend.position="none")+
+  scale_color_manual(values=c("smooth" = "black", "baseline" = "red"))+
+  scale_fill_manual(values=c("smooth" = "black", "baseline" = "red"))+
+  scale_linetype_manual(values=c("smooth" = "solid", "baseline" = "dashed"))
+ggsave("Plots/simulation.pdf", height = 2.5, width = 6)
